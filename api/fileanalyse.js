bdea8d2ebf58e0fe05f2f60c716c62237f9d7a90
nastex21 <nasax2000@gmail.com>
2018-11-30 23:23:01 -0600

    Commit

--- api/fileanalyse.js
+++ api/fileanalyse.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const multer  = require('multer')
-const upload = multer()
+const multer  = require('multer');
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+});
 
-router.post('/', upload.none(), function (req, res, next) {
-    console.log(req.body)
+router.post('/', upload.single('upfile'), function (req, res, next) {
+  console.log(req.body);
+   res.send(req)
   })
 
 module.exports = router;
